25e294374b6ed4217f90d42920830c70001753cb
Petr Pavlu <setup@dagobah.cz>
2020-06-14 12:36:08 +0200

    Update expected value in test_help

--- test/test_cli.py
+++ test/test_cli.py
@@ -75,8 +75,7 @@ class TestCLI(util.StorePassTestCase):
             self.assertEqual(
                 cli_mock.stdout.getvalue(),
                 util.dedent2("""\
-                    |usage: storepass-cli [-h] [-f PASSDB] [-v]
-                    |                     {init,list,show,add,edit,delete,dump} ...
+                    |usage: storepass-cli [-h] [-f PASSDB] [-v] {init,list,show,add,edit,delete,dump} ...
                     |
                     |positional arguments:
                     |  {init,list,show,add,edit,delete,dump}
@@ -84,8 +83,7 @@ class TestCLI(util.StorePassTestCase):
                     |optional arguments:
                     |  -h, --help            show this help message and exit
                     |  -f PASSDB, --file PASSDB
-                    |                        password database file (the default is
-                    |                        ~/.storepass.db)
+                    |                        password database file (the default is ~/.storepass.db)
                     |  -v, --verbose         increase verbosity level
                     """))
             self.assertEqual(cli_mock.stderr.getvalue(), "")
