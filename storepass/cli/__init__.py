a48f4d13f1d1888a3a08a9f0b5ec5f0dcff26a8b
Petr Pavlu <setup@dagobah.cz>
2020-09-17 20:04:37 +0200

    Simplify logging initialization

--- storepass/cli/__init__.py
+++ storepass/cli/__init__.py
@@ -4,23 +4,6 @@
 """Initialization of the CLI module."""
 
 import logging
-import os
-import sys
-
-
-# Create a custom stderr logger. It is same as a handler that would be created
-# by the logging module by default but references sys.stderr at the time when a
-# message is printed which allows sys.stderr to be correctly overwritten in
-# unit tests.
-class _StderrHandler(logging.Handler):
-    def emit(self, record):
-        log_entry = self.format(record)
-        print(log_entry, file=sys.stderr)
-
-
-_log_handler = _StderrHandler()
 
 # Initialize logging.
-logging.basicConfig(format="%(levelname)s: %(name)s: %(message)s",
-                    handlers=[_log_handler])
-_logger = logging.getLogger()
+logging.basicConfig(format="%(levelname)s: %(name)s: %(message)s")
